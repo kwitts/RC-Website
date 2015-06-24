17972a82c81c4a6540c56f6049b717396c78c6ee
Kristi Witts <kristi@kristiwitts.com>
2015-06-24 11:13:25 -0700

    midday updates
    added content to html, styles for new content to css, and js animations
    to some sections. also some images added.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ $('.menu').click(function (e) {
 $('.testimonials-slider').bxSlider({
 	auto: true
 });
+
+$('.slideout').click(function() {
+	$(this).toggleClass('showslide');
+	$(this).children('.content').fadeToggle('fast');
+	$(this).prev().fadeToggle('fast');
+});

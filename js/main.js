16c9caa47152e71e770ca792548e1a8bc83ee57c
Kristi Witts <kristi@kristiwitts.com>
2015-07-30 13:44:24 -0700

    clean up js

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
 /*main.js for Referralclix website - Kristi Witts*/
 
 /*-- GLOBAL --*/
-  /*inview scroll function*/
-  (function($) {
-    function getViewportHeight() {
-      var height = window.innerHeight; // Safari, Opera
-      var mode = document.compatMode;
+/*inview scroll function*/
+(function($) {
+  function getViewportHeight() {
+    var height = window.innerHeight; // Safari, Opera
+    var mode = document.compatMode;
 
-      if ((mode || !$.support.boxModel)) { // IE, Gecko
-        height = (mode == 'CSS1Compat') ?
-          document.documentElement.clientHeight : // Standards
-          document.body.clientHeight; // Quirks
-      }
-      return height;
+    if ((mode || !$.support.boxModel)) { // IE, Gecko
+      height = (mode == 'CSS1Compat') ?
+        document.documentElement.clientHeight : // Standards
+        document.body.clientHeight; // Quirks
     }
+    return height;
+  }
 
-    $(window).scroll(function() {
-      var vpH = getViewportHeight(),
-        scrolltop = (document.documentElement.scrollTop ?
-          document.documentElement.scrollTop :
-          document.body.scrollTop),
-        elems = [];
+  $(window).scroll(function() {
+    var vpH = getViewportHeight(),
+      scrolltop = (document.documentElement.scrollTop ?
+        document.documentElement.scrollTop :
+        document.body.scrollTop),
+      elems = [];
 
-      // naughty, but this is how it knows which elements to check for
-      $.each($.cache, function() {
-        if (this.events && this.events.inview) {
-          elems.push(this.handle.elem);
+    // naughty, but this is how it knows which elements to check for
+    $.each($.cache, function() {
+      if (this.events && this.events.inview) {
+        elems.push(this.handle.elem);
+      }
+    });
+
+    if (elems.length) {
+      $(elems).each(function() {
+        var $el = $(this),
+          top = $el.offset().top,
+          height = $el.height(),
+          inview = $el.data('inview') || false;
+
+        if (scrolltop > (top + height) || scrolltop + vpH < top) {
+          if (inview) {
+            $el.data('inview', false);
+            $el.trigger('inview', [false]);
+          }
+        } else if (scrolltop < (top + height)) {
+          if (!inview) {
+            $el.data('inview', true);
+            $el.trigger('inview', [true]);
+          }
         }
       });
+    }
+  });
+  $(function() {
+    $(window).scroll();
+  });
+})(jQuery);
+(function($) {
+  $.fn.visible = function(partial) {
 
-      if (elems.length) {
-        $(elems).each(function() {
-          var $el = $(this),
-            top = $el.offset().top,
-            height = $el.height(),
-            inview = $el.data('inview') || false;
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
 
-          if (scrolltop > (top + height) || scrolltop + vpH < top) {
-            if (inview) {
-              $el.data('inview', false);
-              $el.trigger('inview', [false]);
-            }
-          } else if (scrolltop < (top + height)) {
-            if (!inview) {
-              $el.data('inview', true);
-              $el.trigger('inview', [true]);
-            }
-          }
-        });
-      }
-    });
-    $(function() {
-      $(window).scroll();
-    });
-  })(jQuery);
-  (function($) {
-    $.fn.visible = function(partial) {
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  };
+})(jQuery);
 
-        var $t            = $(this),
-            $w            = $(window),
-            viewTop       = $w.scrollTop(),
-            viewBottom    = viewTop + $w.height(),
-            _top          = $t.offset().top,
-            _bottom       = _top + $t.height(),
-            compareTop    = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
 
-      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-    };
-  })(jQuery);
+var currentYear = (new Date).getFullYear();
+
+$(document).ready(function() {
 
   /*current year function - for copyright footer*/
-  var currentYear = (new Date).getFullYear();
-  $(document).ready(function() {
-    $(".year").text( (new Date).getFullYear() );
-  });
+  $(".year").text( (new Date).getFullYear() );
 
   /*mobile navigation menu icon*/
   $('.menu').click(function(e) {
@@ -94,9 +96,10 @@
     $('.overlay').fadeOut("slow");
     e.preventDefault();
   });
+});
 
 /*-- HOMEPAGE --*/
-
+$(document).ready(function() {
   /*testimonials/quotes slider*/
   $('.testimonials-slider').bxSlider({
     auto: true
@@ -165,9 +168,11 @@
     });
 
   });
+});
 
 /*-- HOW IT WORKS PAGE --*/
 /*-- (minus "mike makes a purchase", see story.js) --*/
+$(document).ready(function() {
 
   /*spinning referral funnel*/
   $('.funnelspin').load(function() {
@@ -178,7 +183,10 @@
     $('.bottom3').delay(2900).animate({opacity:'1'},1000);
   });
 
+});
+
 /*-- ANALYTICS PAGE --*/
+$(document).ready(function() {
 
   /*analytics funnel*/
   $('.insights a').click(function(e) {
@@ -198,3 +206,5 @@
       $(this).animate({'width': number + '%'}, 1000);
     });
   });
+  
+});

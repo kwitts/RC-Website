f9c068444b6c247fb2edc8f2acaf36ec015bf8cd
Kristi Witts <kristi@kristiwitts.com>
2015-07-30 10:11:45 -0700

    transition and step two complete
    work on next transition and step three

--- js/story.js
+++ js/story.js
@@ -1,33 +1,88 @@
 /*story.js for Referralclix How It Works page - Kristi Witts*/
 /*-- "Mike Makes a Purchase" --*/
 
+/*--LOADER IMG--*/
+// $(window).load(function() {
+// 	$(".loader").fadeOut("slow");
+// })
+
+/*DEV PURPOSES ONLY*/
+$(document).scroll(function() {
+    console.log($(document).scrollTop());
+})
+
+/*--SCROLL ANIMATIONS ON DOC LOAD--*/
 $(document).ready(function() {
 
-  /* Scroll positions */
+  /* SCROLL POSITIONS */
   $(window).scroll(function() {
 
-    /* Mike - Step One and Two */
-    if ($(this).scrollTop() > 0) {
-      $('#mike').css({ 'position': 'fixed', 'top': '340px', 'opacity': '1' });
+    /* MIKE - Step One and Two */
+    if ($(this).scrollTop() > 1400) {
+      $('#mike').css({ 'position': 'absolute', 'top': '1240px' });
+    } else if ($(this).scrollTop() > 200) {
+      $('#mike').css({ 'position': 'fixed', 'top': '220px' });
     } else {
-      $('#mike').css({ 'position': 'absolute', 'top': '0px', 'opacity': '0.6' });
+      $('#mike').css({ 'position': 'absolute', 'top': '0px' });
     }
-    // if ($(this).scrollTop() > 150) {
-    //   $('#mike').css({ 'transform': 'translateX(-100px)' });
+    if ($(this).scrollTop() > 700) {
+      $('#mike').css({ 'transform': 'translateX(-150px) scale(0.8)' });
+      $('#mike').css({ '-webkit-transform': 'translateX(-150px) scale(0.8)' });
+    } else {
+      $('#mike').css({ 'transform': 'translateX(0) scale(1)' });
+      $('#mike').css({ '-webkit-transform': 'translateX(0) scale(1)' });
+    }
+
+    /* SUZIE - Step Two, Three, and Four */
+    if ($(this).scrollTop() > 910) {
+      $('#suzie').css({ 'position': 'fixed', 'top': '220px' });
+      // $('#suzie').css({ 'transform': 'translateX(150px) scale(0.8)' });
+      // $('#suzie').css({ '-webkit-transform': 'translateX(150px) scale(0.8)' });
+    } else {
+      $('#suzie').css({ 'position': 'absolute', 'top': '100px' });
+    }
+
+     /* MIKE'S FRIENDS - Step Two */
+    if ($(this).scrollTop() > 1400) {
+      $('#mikesfriends').css({ 'position': 'absolute', 'top': '510px' });
+    } else if ($(this).scrollTop() > 910) {
+      $('#mikesfriends').css({ 'position': 'fixed', 'top': '120px' });
+    } else {
+      $('#mikesfriends').css({ 'position': 'absolute', 'top': '0px' });
+    }
+
+    /* REFERRAL PLANE - Step Two*/
+    if ($(this).scrollTop() > 950) {
+      $('#plane-right').css({ 'position': 'fixed', 'top': '320px' });
+    } else {
+      $('#plane-right').css({ 'position': 'absolute', 'top': '0px' });
+    }
+    if ($(this).scrollTop() > 1000) {
+      $('#plane-right').css({ 'transform': 'translateX(150px)' });
+      $('#plane-right').css({ '-webkit-transform': 'translateX(150px)' });
+    } else {
+      $('#plane-right').css({ 'transform': 'translateX(-150px)' });
+      $('#plane-right').css({ '-webkit-transform': 'translateX(-150px)' });
+    }
+
+    // if ($(this).scrollTop() > 700) {
+    //   $('#suzie').css({ 'transform': 'translateX(150px) scale(0.8)' });
+    //   $('#suzie').css({ '-webkit-transform': 'translateX(150px) scale(0.8)' });
     // } else {
-    //   $('#mike').css({ 'transform': 'translateX(0)' });
+    //   $('#suzie').css({ 'transform': 'translateX(0) scale(1)' });
+    //   $('#suzie').css({ '-webkit-transform': 'translateX(0) scale(1)' });
     // }
 
 
   });
 
-  /* Change Image */
+  /* CHANGE IMAGE */
   $(window).scroll(function() {
 
-    /* Mike - Step One and Two */
-    if ($(this).scrollTop() > 700) {
+    /* MIKE - Step One and Two */
+    if ($(this).scrollTop() > 500) {
       $('#mike').css('background', 'url("../images/mike_03.png")');
-    } else if ($(this).scrollTop() > 200) {
+    } else if ($(this).scrollTop() > 350) {
       $('#mike').css('background', 'url("../images/mike_02.png")');
     } else {
       $('#mike').css('background', 'url("../images/mike_01.png")');
@@ -35,22 +90,64 @@ $(document).ready(function() {
 
   });
 
-  /* Disappear */
+  /* APPEAR/DISAPPEAR */
   $(window).scroll(function() {
+    /*MIKE*/
     if ($(this).scrollTop() > 4500) {
       $('#mike').css({ 'opacity': '0' });
     } else {
       $('#mike').css({ 'opacity': '1' });
     }
+
+    /*SUZIE*/
+
+    /*MIKE*/
+    if ($(this).scrollTop() > 910) {
+      $('#suzie').css({ 'opacity': '1' });
+    } else {
+      $('#suzie').css({ 'opacity': '0' });
+    }
+
+    /*MIKE'S FRIENDS*/
+    if ($(this).scrollTop() > 910) {
+      $('#mikesfriends').css({ 'opacity': '1' });
+    } else {
+      $('#mikesfriends').css({ 'opacity': '0' });
+    }
+
+    /*REFERRAL PLANE - MIKE TO FRIENDS*/
+    if ($(this).scrollTop() > 1200) {
+      $('#plane-right').css({ 'opacity': '0' });
+    } else if ($(this).scrollTop() > 1000) {
+      $('#plane-right').css({ 'opacity': '1' });
+    } else {
+      $('#plane-right').css({ 'opacity': '0' });
+    }
   });
 
-  /* Show/Hide Content */
+  /* SHOW/HIDE/POSITION TEXT CONTENT */
   $(window).scroll(function() {
   	var mymessage = $('#mikepurchase');
-    if ($(this).scrollTop() > 200) {
+    if ($(this).scrollTop() > 740) {
+      mymessage.fadeOut('500');
+      mymessage.css({'position': 'absolute', 'top': '0'});
+    } else if ($(this).scrollTop() > 320) {
       mymessage.fadeIn('500');
+      mymessage.css({'position': 'fixed', 'top': '130px'});
     } else {
-      mymessage.fadeOut('200')
+      mymessage.fadeOut('200');
+    }
+  });
+  $(window).scroll(function() {
+  	var mymessage = $('#mikerefers');
+    if ($(this).scrollTop() > 1400) {
+      mymessage.fadeOut('500');
+      mymessage.css({'position': 'absolute', 'top': '0'});
+    } else if ($(this).scrollTop() > 800) {
+      mymessage.fadeIn('500');
+      mymessage.css({'position': 'fixed', 'top': '130px'});
+    } else {
+      mymessage.fadeOut('200').css({'positon': 'fixed', 'top': '130px'})
     }
   });
   // function isScrolledIntoView(elem) {
